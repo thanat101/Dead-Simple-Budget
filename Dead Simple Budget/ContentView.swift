b972acee03706d76e210cf760d249598368780bd
mp <thanat101@gmail.com>
2026-03-07 20:00:36 -0500

    Initial Commit

---
+++ Dead Simple Budget/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Dead Simple Budget
+//
+//  Created by Mark Pfeifer on 3/7/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Dead Simple Budget/Dead_Simple_BudgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dead_Simple_BudgetApp.swift
+//  Dead Simple Budget
+//
+//  Created by Mark Pfeifer on 3/7/26.
+//
+
+import SwiftUI
+
+@main
+struct Dead_Simple_BudgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
